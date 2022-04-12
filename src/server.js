8be1cc6317d88623b12ef10c48ae7ee7a695ab14
bignera <martinus.cr7@gmail.com>
2022-04-12 11:53:06 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 /* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
@@ -12,8 +15,16 @@ const init = async () => {
       },
     },
   });
+  //  // registrasi satu plugin
+   await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
-  server.route(routes);
+  // server.route(routes);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
