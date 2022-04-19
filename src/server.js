05bdabd71d96fa54ebf9b8e8b464533e5136f3ee
bignera <martinus.cr7@gmail.com>
2022-04-19 13:29:06 +0700

    User controller

--- src/server.js
+++ src/server.js
@@ -6,8 +6,15 @@ const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+ 
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -18,14 +25,24 @@ const init = async () => {
     },
   });
   //  // registrasi satu plugin
-   await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
+  
   // server.route(routes);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
