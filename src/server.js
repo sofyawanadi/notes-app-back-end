b3414629cd1a861373d680ece3039b771123d1d4
bignera <martinus.cr7@gmail.com>
2022-04-20 10:14:19 +0700

    autentikasi

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const notes = require('./api/notes');
 // const NotesService = require('./services/inMemory/NotesService');
 const NotesService = require('./services/postgres/NotesService');
@@ -10,11 +11,17 @@ const NotesValidator = require('./validator/notes');
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
- 
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -24,9 +31,34 @@ const init = async () => {
       },
     },
   });
-  //  // registrasi satu plugin
+  
+
+  // registrasi plugin eksternal
   await server.register([
     {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
+  //  // registrasi satu plugin
+  await server.register([{
       plugin: notes,
       options: {
         service: notesService,
@@ -38,6 +70,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
